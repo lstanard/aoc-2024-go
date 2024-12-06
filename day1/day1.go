900bb1c779c44b36bc143e18c2757a8dc2f74e01
Charlie Stanard <charlie.stanard@guild.com>
2024-12-05 18:43:38 -0600

    Init go project

---
+++ day1/day1.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "aoc-2024-go/read_input"
+)
+
+func main() {
+  data := read_input.ReadInputFile()
+  fmt.Println(data)
+}
---
+++ read_input/read_input.go
@@ -0,0 +1,10 @@
+package read_input
+
+import "fmt"
+
+func ReadInputFile() string {
+	fmt.Println("Reading input file...")
+
+	data := "Hello, World!"
+  return data
+}
