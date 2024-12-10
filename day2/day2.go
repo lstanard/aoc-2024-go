48f3326e63296e8e286cfd3a0f2d1d961b023606
Charlie Stanard <charlie.stanard@guild.com>
2024-12-09 20:04:01 -0600

    Finish day 2 part 1

--- day2/day2.go
+++ day2/day2.go
@@ -1,8 +1,12 @@
 package day2
 
-import "aoc-2024-go/utils"
+import (
+	"aoc-2024-go/utils"
+	"strconv"
+	"strings"
+)
 
-func ParseInput(sample bool) ([]string) {
+func ParseInput(sample bool) []string {
   path := ""
   if sample {
     path = "input-sample.txt"
@@ -17,4 +21,61 @@ func ParseInput(sample bool) ([]string) {
 
   lines := utils.SplitInputToLines(data)
   return lines
+}
+
+func isLineSafe(line []string, direction string) bool {
+  // down to the last number, return true if we made it this far
+  if len(line) < 2 {
+    return true
+  }
+
+  num1, _ := strconv.Atoi(line[0])
+  num2, _ := strconv.Atoi(line[1])
+
+  if num1 > num2 {
+    // decreasing
+    if direction == "up" {
+      return false
+    }
+    if num1 - num2 > 3 {
+      return false
+    }
+    return isLineSafe(line[1:], "down")
+  } else if num1 < num2 {
+    // increasing
+    if direction == "down" {
+      return false
+    }
+    if num2 - num1 > 3 {
+      return false
+    }
+    return isLineSafe(line[1:], "up")
+  } else {
+    // the two numbers are equal
+    return false
+  }
+}
+
+func getSafeCount(lines []string) int {
+  safeCount := 0
+  for _, line := range lines {
+    if len(line) == 0 {
+      continue
+    }
+
+    sNums := strings.Fields(line)
+    safe := isLineSafe(sNums, "")
+    if safe {
+      safeCount++
+    }
+  }
+
+  return safeCount
+}
+
+func Part1(sample bool) int {
+  lines := ParseInput(sample)
+  safeCount := getSafeCount(lines)
+
+  return safeCount
 }
---
+++ day2/day2_test.go
@@ -0,0 +1,27 @@
+package day2_test
+
+import (
+  "aoc-2024-go/day2"
+  "testing"
+)
+
+func TestPart1_SampleData(t *testing.T) {
+  result := day2.Part1(true)
+  if result != 2 {
+    t.Errorf("Expected 2, got %d", result)
+  }
+}
+
+func TestPart1(t *testing.T) {
+  result := day2.Part1(false)
+  if result != 2 {
+    t.Errorf("Expected 2, got %d", result)
+  }
+}
+
+// func TestPart2(t *testing.T) {
+//   result := day1.Part2()
+//   if result != 23609874 {
+//     t.Errorf("Expected 23609874, got %d", result)
+//   }
+// }
