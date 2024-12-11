ef00e632090ab3dd206e0b8facfe66a1a581486e
Charlie Stanard <charlie.stanard@guild.com>
2024-12-10 19:27:57 -0600

    Finish day 2 part 2

--- day2/day2.go
+++ day2/day2.go
@@ -2,80 +2,102 @@ package day2
 
 import (
 	"aoc-2024-go/utils"
+	"slices"
 	"strconv"
 	"strings"
 )
 
 func ParseInput(sample bool) []string {
-  path := ""
-  if sample {
-    path = "input-sample.txt"
-  } else {
-    path = "input.txt"
-  }
-  success, data := utils.ReadInputFile(path)
+	path := ""
+	if sample {
+		path = "input-sample.txt"
+	} else {
+		path = "input.txt"
+	}
+	success, data := utils.ReadInputFile(path)
 
-  if !success {
-    return nil
-  }
+	if !success {
+		return nil
+	}
 
-  lines := utils.SplitInputToLines(data)
-  return lines
+	lines := utils.SplitInputToLines(data)
+	return lines
 }
 
 func isLineSafe(line []string, direction string) bool {
-  // down to the last number, return true if we made it this far
-  if len(line) < 2 {
-    return true
-  }
+	// down to the last number, return true if we made it this far
+	if len(line) < 2 {
+		return true
+	}
 
-  num1, _ := strconv.Atoi(line[0])
-  num2, _ := strconv.Atoi(line[1])
+	num1, _ := strconv.Atoi(line[0])
+	num2, _ := strconv.Atoi(line[1])
 
-  if num1 > num2 {
-    // decreasing
-    if direction == "up" {
-      return false
-    }
-    if num1 - num2 > 3 {
-      return false
-    }
-    return isLineSafe(line[1:], "down")
-  } else if num1 < num2 {
-    // increasing
-    if direction == "down" {
-      return false
-    }
-    if num2 - num1 > 3 {
-      return false
-    }
-    return isLineSafe(line[1:], "up")
-  } else {
-    // the two numbers are equal
-    return false
-  }
+	if num1 > num2 {
+		// decreasing
+		if direction == "up" {
+			return false
+		}
+		if num1-num2 > 3 {
+			return false
+		}
+		return isLineSafe(line[1:], "down")
+	} else if num1 < num2 {
+		// increasing
+		if direction == "down" {
+			return false
+		}
+		if num2-num1 > 3 {
+			return false
+		}
+		return isLineSafe(line[1:], "up")
+	} else {
+		// the two numbers are equal
+		return false
+	}
 }
 
-func getSafeCount(lines []string) int {
-  safeCount := 0
-  for _, line := range lines {
-    if len(line) == 0 {
-      continue
-    }
+func getSafeCount(lines []string, useDampener bool) int {
+	safeCount := 0
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
 
-    sNums := strings.Fields(line)
+		sNums := strings.Fields(line)
     safe := isLineSafe(sNums, "")
-    if safe {
-      safeCount++
-    }
-  }
 
-  return safeCount
+    // re-check all unsafe lines
+    if (useDampener && !safe) {
+      for i := range sNums {
+        subSet := make([]string, len(sNums))
+        copy(subSet, sNums)
+        subSet = slices.Delete(subSet, i, i+1)
+        subSetSafe := isLineSafe(subSet, "")
+
+        if (subSetSafe) {
+          safe = true
+        }
+      }
+    }
+
+		if safe {
+			safeCount++
+		}
+	}
+	return safeCount
 }
 
 func Part1(sample bool) int {
-  lines := ParseInput(sample)
-  safeCount := getSafeCount(lines)
+	lines := ParseInput(sample)
+	safeCount := getSafeCount(lines, false)
 
-  return safeCount
+	return safeCount
+}
+
+func Part2(sample bool) int {
+	lines := ParseInput(sample)
+	safeCount := getSafeCount(lines, true)
+
+	return safeCount
 }
--- day2/day2_test.go
+++ day2/day2_test.go
@@ -15,13 +15,20 @@ func TestPart1_SampleData(t *testing.T) {
 func TestPart1(t *testing.T) {
   result := day2.Part1(false)
   if result != 213 {
-    t.Errorf("Expected 2, got %d", result)
+    t.Errorf("Expected 213, got %d", result)
   }
 }
 
-// func TestPart2(t *testing.T) {
-//   result := day1.Part2()
-//   if result != 23609874 {
-//     t.Errorf("Expected 23609874, got %d", result)
-//   }
-// }
+func TestPart2_SampleData(t *testing.T) {
+  result := day2.Part2(true)
+  if result != 4 {
+    t.Errorf("Expected 4, got %d", result)
+  }
+}
+
+func TestPart2(t *testing.T) {
+  result := day2.Part2(false)
+  if result != 285 { // it's between 273 and 295
+    t.Errorf("Expected 4, got %d", result)
+  }
+}
