fdd3f697f2a190e35c87a0aa51b61b837aaafc89
Charlie Stanard <charlie.stanard@guild.com>
2024-12-12 19:17:48 -0600

    WIP day 4 part 1

--- day4/day4.go
+++ day4/day4.go
@@ -22,43 +22,68 @@ func parseInput(sample bool) []string {
 	return lines
 }
 
+func validateDirection(i, j int, dir [2]int, rows, cols int) bool {
+	// Can't move up or down
+	if i-dir[1] < 0 || i+dir[1] >= rows {
+		return false
+	}
+	// Can't move left or right
+	if j-dir[0] < 0 || j+dir[0] >= cols {
+		return false
+	}
+	return true
+}
+
 // Grid traversal / depth-first search
 // https://www.youtube.com/watch?v=w8DYqrWjX1w
 func Part1(sample bool) int {
-  lines := parseInput(sample)
-  fmt.Println(lines)
+	lines := parseInput(sample)
+	fmt.Println(lines)
 
-  // Directions for grid traversal
-  // directions := [8][2]int{
-  //   {0,1},    // up
-  //   {1,1},    // up-right
-  //   {1,0},    // right
-  //   {-1,0},   // left
-  //   {-1,-1},  // down-left
-  //   {0,-1},   // down
-  //   {1,-1},   // down-right
-  //   {-1,1},   // up-left
-  // }
-  // OR word := "XMAS"? Depends
-  letters := [4]string{"X", "M", "A", "S"}
-  letterIndex := 0
-  rows := len(lines)
-  // cols := len(lines[0])
-  // visited := make([][]bool, rows)
+	// Directions for grid traversal
+  // directions are [x, y]
+	directions := [8][2]int{
+		{0, 1},   // down
+		{1, 1},   // right-down
+		{1, 0},   // right
+    {1, -1},  // right-up
+    {0, -1},  // up
+    {-1, 1},  // left-up
+		{-1, 0},  // left
+		{-1, -1}, // left-down
+	}
+	// OR word := "XMAS"? Depends
+	letters := [4]string{"X", "M", "A", "S"}
+	letterIndex := 0
+	rows := len(lines) - 1 // this isn't great but there's an extra empty line that I should strip out
+	cols := len(lines[0])
+	// visited := make([][]bool, rows)
 
-  // Loop through each row
-  for i := 0; i < rows; i++ {
-    // Loop through each character in the row
-    row := lines[i]
-    for j := 0; j < len(row); j++ {
-      // OR char := lines[i][j]
-      char := string(row[j])
-      if char == letters[letterIndex] {
-        // for _, dir := range directions {
-        // }
-      }
-    }
-  }
+	// Loop through each row
+	// Loop through each character in the row
+	// Loop through each direction
+	// Loop through each character in the word
+	// Store cooridnates where each word is found (x, y, direction)
 
-  return 0
+	// NOTE: In this case i = x and j = y
+	for i := 0; i < rows; i++ {
+		// fmt.Println("Row", lines[i])
+		for j := 0; j < cols; j++ {
+			if (lines[i]) == "" {
+				continue
+			}
+			char := string(lines[i][j])
+			if char == letters[letterIndex] {
+				fmt.Println("Found", char, "at", i, j)
+				for _, dir := range directions {
+					validDirection := validateDirection(i, j, dir, rows, cols)
+          if (validDirection) {
+            fmt.Println("Valid direction", dir)
+          }
+				}
+			}
+		}
+	}
+
+	return 0
 }
